100b99a49101bc9944d93267d0f0ff5e2826f002
Oscar Roa <oshardy3@gmail.com>
2015-11-25 14:03:01 -0600

    Foodtruck model and serializer added.

--- foodtrucks/models.py
+++ foodtrucks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
-# Create your models here.
+
+class Foodtruck(models.Model):
+    city = models.CharField(max_length=50)
+    facebook = models.URLField()
+    food_type = models.CharField(max_length=100)
+    location = PlainLocationField(based_fields=[city], zoom=7)
+    name = models.CharField(max_length=100)
+    photo = models.ImageField()
+    price = models.IntegerField()
+    rating = models.IntegerField()
+    twitter = models.URLField()
