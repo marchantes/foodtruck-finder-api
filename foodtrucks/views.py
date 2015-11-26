29f3d435a3cfcdf0a60d2e1e73943ce4602de5f8
Oscar Roa <oshardy3@gmail.com>
2015-11-25 20:15:58 -0600

    Foodtrucks detail view

--- foodtrucks/views.py
+++ foodtrucks/views.py
@@ -6,3 +6,8 @@ from foodtrucks.serializers import FoodtruckSerializer
 class FoodtruckList(generics.ListCreateAPIView):
     queryset = Foodtruck.objects.all()
     serializer_class = FoodtruckSerializer
+
+
+class FoodtruckDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Foodtruck.objects.all()
+    serializer_class = FoodtruckSerializer
