8671971d2bbc0729d51b2e2c6463fb08391b4065
Oscar Roa <oshardy3@gmail.com>
2015-11-25 21:46:33 -0600

    Location returned as a JSON.

--- foodtrucks/models.py
+++ foodtrucks/models.py
@@ -12,3 +12,16 @@ class Foodtruck(models.Model):
     price = models.IntegerField()
     rating = models.IntegerField()
     twitter = models.URLField()
+
+    def __str__(self):
+        return "\nName: {}\nRating: {}\nFoodtype: {}\n".format(self.name,
+                                                               self.rating,
+                                                               self.food_type)
+
+    @property
+    def location_parser(self):
+        location_object = {}
+        data = self.location.split(',')
+        location_object['lat'] = data[0]
+        location_object['long'] = data[1]
+        return location_object
