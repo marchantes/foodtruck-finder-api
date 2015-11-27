3f03de0ba255677843dc6be7257ca5cde9beaaa0
Jorge Andrade <jorandradefig@gmail.com>
2015-11-26 21:44:25 -0600

    add/users application

--- foodtruckers_api/settings.py
+++ foodtruckers_api/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
 
     #   Model's location field
     'location_field',
---
+++ users/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from location_field.models.plain import PlainLocationField
+from django.contrib.auth.models import User
