4d2a164ef4449e49cbe97e918a9dd9a3589eb511
Oscar Roa <oshardy3@gmail.com>
2015-12-02 02:06:38 -0600

    Delete: Migrations, so there will be no conflicts because of the custom user model Add: Custom user model 'UserProfile' with all its dependencies and methods Replace: All relations to Django default user model with UserProfile model.

--- foodtruckers_api/urls.py
+++ foodtruckers_api/urls.py
@@ -1,15 +1,12 @@
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 
-urlpatterns = patterns('',
+urlpatterns = [
     #   API Endpoints
     url(r'^api/v1/', include('foodtrucks.urls',
                              namespace='foodtrucks')),
     url(r'^api/v1/', include('users.urls', namespace='users')),
- )
-
-admin_patterns = [
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
